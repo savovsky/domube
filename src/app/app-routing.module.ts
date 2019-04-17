bf40ef4ff477fe80ecdf5aef9117659d7d8bd3c1
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-04-17 10:37:52 +0300

    Classes page - created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'classes', pathMatch: 'full' },
+  {
+    path: 'classes',
+    loadChildren: './classes/classes.module#ClassesPageModule'
+  }
 ];
 
 @NgModule({
